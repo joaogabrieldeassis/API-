7fdefcbe902f82256c61563143020507d1b5e50d
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-30 10:23:22 -0300

    Migração deu certo

---
+++ shop/Shop/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+
+namespace Shop.Controllers
+{
+    [Route("products")]
+    public class ProductController:ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x=>x.Category).AsNoTracking().ToListAsync();
+            return Ok(products);
+        }
+        [HttpGet]
+        [Route("id:int")]
+        public async Task<IActionResult> GetId(int id,[FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(x => x.Category).AsNoTracking().FirstOrDefaultAsync(x=>x.Id==id);
+            return Ok(products);
+        }
+    }
+}
