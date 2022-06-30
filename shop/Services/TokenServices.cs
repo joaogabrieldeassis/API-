8d4a0954d0b3f1583294e107ba8fde7fd3a79f2a
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-30 17:47:51 -0300

    Iniciando o autenticação e autorização

---
+++ shop/Services/TokenServices.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Shop.Model;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Shop.Services
+{
+    public static class TokenServices
+    {
+        public static string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Secrete);
+            var tokenDescription = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role),
+                }),
+                Expires = DateTime.UtcNow.AddDays(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+               var token = tokenHandler.CreateToken(tokenDescription);
+               return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ shop/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secrete = "fedaf7d345G8863b48e197b9287d492b708e";
+    }
+}
