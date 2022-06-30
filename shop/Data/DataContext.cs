3617a26f79c18d76610e6dadc80af7d79a998905
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-06-30 14:51:06 -0300

    Restauração no projeto

---
+++ shop/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Model;
+
+namespace Shop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=localhost,1433;Database=Shop;User ID=sa;Password=1q2w3e4r@#$");
+    }
+}
---
+++ shop/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+ConfigureAuthentication(builder);
+ConfigureMVC(builder);
+ConfigureServices(builder);
+
+var app = builder.Build();
+LoadConfiguration(app);
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseStaticFiles();
+app.MapControllers();
+app.Run();
+
+//Método para configurar o meu appsettings.json
+void LoadConfiguration(WebApplication app)
+{
+    
+}
+
+//Método da Configuração da minha Autenticação e autorização
+void ConfigureAuthentication(WebApplicationBuilder builder)
+{
+    
+}
+
+//Método para Desabilitar a validação automatica da minha api
+void ConfigureMVC(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
+}
+
+//Método para serviços Do banco e Token
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers();
+    //builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+    builder.Services.AddDbContext<DataContext>();
+    builder.Services.AddScoped<DataContext, DataContext>();
+    builder.Services.AddMvc();
+}
